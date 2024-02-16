790be99a62212344acd37c2412947f40cdffeeef
Lulu828-cyber <146968254+Lulu828-cyber@users.noreply.github.com>
2024-02-16 19:48:21 +0000

    Done.Challenge

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-//cada uno tiene que tener una posicion. 
+//cada uno tiene que tener una posicion.
 // pronoun 2/2/2/2--4/4/4/4--1/1/1/1
-
-for(let a=0; a < pronoun.length; a++){
-    for(let b=0; b < adj.length; b++) 
-    for(let c=0; c < noun.length; c++) 
-        console.log(`${pronoun[a]}${adj[b]}${noun[c]}.com`);
-    }
+function generadorDominios(p, ad, n) {
+  for (let a = 0; a < p.length; a++) {
+    for (let b = 0; b < ad.length; b++)
+      for (let c = 0; c < n.length; c++)
+        console.log(`${p[a]}${ad[b]}${n[c]}.com`);
+  }
+}
+generadorDominios(pronoun, adj, noun);
